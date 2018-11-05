a7a0064aaa91c928456cff916ade71dd3d03fc77
arimunoz <munoza371@gmail.com>
2018-11-04 22:06:49 -0600

    added presentation to branch and other stuff to work on

--- campData_project/app/models/emergency_contact.rb
+++ campData_project/app/models/emergency_contact.rb
@@ -1,2 +1,3 @@
 class EmergencyContact < ApplicationRecord
+	belongs_to :camp_members
 end
--- campData_project/config/routes.rb
+++ campData_project/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'camp_members#index'
   get 'pages/login'
   get 'pages/registration'
   get 'pages/admin_page'
@@ -7,4 +8,7 @@ Rails.application.routes.draw do
   resources :emergency_contacts
   resources :camp_members
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  
+  
 end
